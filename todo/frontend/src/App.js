b264a83bd54d6e7f5e308b6feb2c77cee1df9d91
Dmuskeeter <tdheeraj9a@gmail.com>
2023-02-10 21:26:02 +0530

    settingup the frontend

---
+++ todo/frontend/src/App.js
@@ -0,0 +1,17 @@
+import react, { Component } from "react";
+
+// functional components:
+
+// const App=()=>{
+//     return(
+//         <h1>Hey! Dheeraj</h1>
+//     )
+// }
+
+// class commponent
+class App extends Component {
+  render() {
+    return <h1>Hey! Dheeraj BABA</h1>;
+  }
+}
+export default App;
