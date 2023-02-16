9b06042182bb5957a54026a7d153b34e0e2ccc61
Dmuskeeter <tdheeraj9a@gmail.com>
2023-02-16 21:55:30 +0530

    todo forms

--- todo/frontend/src/Components/TodoForm.js
+++ todo/frontend/src/Components/TodoForm.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoForm = () => {
-  return <>FORM</>;
+  const [value, setValue] = useState("");
+
+  const onFormSubmit = () => {};
+
+  const onInputChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={onFormSubmit}>
+        <input
+          placeholder="Add a Todo to get started..."
+          className="input"
+          onChange={onInputChange}
+          autofocus
+        />
+      </form>
+    </>
+  );
 };
 
 export default TodoForm;
