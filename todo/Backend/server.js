aafe5916dee7ff77dd2aabfa7243a5198c03d4dc
Dmuskeeter <tdheeraj9a@gmail.com>
2023-02-19 18:07:18 +0530

    cross origin resource hasbeen added

--- todo/Backend/server.js
+++ todo/Backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = require("./config/db");
@@ -7,6 +8,7 @@ const app = express();
 connectDB();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.get("/v1", (req, res) => {
   res.send("<h1>home page!</h1>");
 });
